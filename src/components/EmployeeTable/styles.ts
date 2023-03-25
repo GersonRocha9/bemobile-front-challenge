86b2cba359e42812bc1abdde272873e63766e40e
Gerson Rocha <gersonrocha9@gmail.com>
2023-03-25 11:32:03 -0300

    fix: background color on employees table

--- src/components/EmployeeTable/styles.ts
+++ src/components/EmployeeTable/styles.ts
@@ -1,8 +1,19 @@
 import styled from 'styled-components'
 
 export const Table = styled.table`
+  border-collapse: collapse;
   text-align: center;
   width: 100%;
+  overflow: hidden;
+  border: 1px solid #000;
+
+  -webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  border-radius: 4px;
+
+  -webkit-box-shadow: 0 1px 0 rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: 0 1px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 1px 0 rgba(0, 0, 0, 0.2);
 `
 
 export const Th = styled.th`
@@ -13,7 +24,9 @@ export const Th = styled.th`
 
 export const Td = styled.td`
   box-shadow: 0 1px 0 rgba(0, 0, 0, 0.2);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
   padding: 8px 32px;
+  background: ${({ theme }) => theme.colors.white};
 `
 
 export const Img = styled.img`
